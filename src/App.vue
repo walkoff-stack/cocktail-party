76ff8928669ef842e9d2efde921a1ad1d1f921be
Igor Walkoff <bywalkoff@gmail.com>
2025-09-08 15:25:45 +0300

    chore: add base layout structure
    - Template areas provided to App component
    
    - NavSidebar component created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavSidebar from '@/shared/components/layout/sidebar/NavSidebar.vue';
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="layout container">
+    <nav-sidebar></nav-sidebar>
+    <main class="content">
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+@use '@/shared/styles/container';
+@use '@/shared/styles/mixins';
+.layout {
+  display: grid;
+  grid-template-areas: 'content';
+  grid-template-columns: 1fr;
+  min-height: 100dvh;
+  position: relative;
+
+  @include res('desktop') {
+    grid-template-areas: 'sidebar content';
+    grid-template-columns: 300px 1fr;
+  }
+}
+
+.content {
+  grid-area: content;
+  min-height: 100dvh;
+  padding: 2rem 1rem;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/shared/components/layout/sidebar/NavSidebar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="nav-sidebar"></nav>
+</template>
+
+<style lang="scss">
+.nav-sidebar {
+  grid-area: sidebar;
+  padding: 2rem 1rem;
+  top: 0;
+  height: 100dvh;
+  box-sizing: border-box;
+}
+</style>
